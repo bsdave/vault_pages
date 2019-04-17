816013a6ea21682266ec4dccc2f650c4dd024935
Sergey Balabanov <sergeibalabanov@gmail.com>
2019-04-18 02:06:23 +0600

    Page about us

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -21,4 +21,50 @@ $(function () {
     )
   })
 
+  $('.horizontal-scrolling-controls a').bind('click', function (event) {
+    event.preventDefault();
+    $('html, body').stop();
+    const anchor = $(this).attr('href');
+
+    moveToGraduate(anchor);
+
+    $('.control').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  activateFirstGraduate();
+
+  $('.certificate').on('click', function () {
+    // $('.certificate').toggleClass('active');
+    $(this).toggleClass('active');
+  });
 });
+
+const moveToGraduate = (anchor) => {
+  const graduateNumber = anchor.match(/\d+/g).map(Number)[0];
+
+  $('.horizontal-scrolling-content').stop().animate({
+    scrollLeft: findGraduatePosition(graduateNumber)
+  }, 1000);
+}
+
+const getContainerIndent = (container) => {
+  const margin = parseInt(container.css('margin-left'));
+  const padding = parseInt(container.css('padding-left'));
+
+  return margin + padding;
+}
+
+const findGraduatePosition = (graduateNumber) => {
+  const indent = getContainerIndent($('.graduates-box .container'));
+  $('.graduate').css('margin-right', indent);
+
+  const graduateFullWidth = $('.graduate').width() + indent;
+
+  return (graduateNumber - 1) * graduateFullWidth;
+}
+
+const activateFirstGraduate = () => {
+  moveToGraduate('#graduate1');
+  $('.control:first-child').addClass('active');
+}
